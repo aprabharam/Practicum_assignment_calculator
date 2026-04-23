c6f730074c1f98ecb3266db87819108bd70703cb
aprabha7 <aprabharam@gmail.com>
2026-04-23 14:26:01 +0200

    Added evaluate, clear display and backspace features

--- script.js
+++ script.js
@@ -53,13 +53,77 @@ function appendNumber(number) {
     if(display.textContent === "0" || shouldResetDisplay) {
         updateDisplayValue(number);
         shouldResetDisplay = false;
-        // console.log("display", display.textContent);
+        console.log("display1", display.textContent);
     } else {
         updateDisplayValue(display.textContent + number);
-        // console.log("display", display.textContent);
+        console.log("display2", display.textContent);
     }
+}
+
+function setOperator(operator) {
+    if(operatorSelected !== null) {
+        evaluate();
+    }
+
+    firstNumber = display.textContent;
+    operatorSelected = operator;
+    shouldResetDisplay = true;
+    console.log("first number, op", display.textContent, operator);
+}
+
+function evaluate() {
+    if(operatorSelected === null || shouldResetDisplay) {
+        return;
+    }
+    secondNumber = display.textContent;
+    console.log("second number", secondNumber);
+
+    let result = operate(operatorSelected, firstNumber, secondNumber);
+    console.log("result", result);
+
+    //     // handle divide by zero message
+    // if (typeof result === "string") {
+    //     updateDisplay(result);
+    //     operator = null;
+    //     return;
+    // }
+
+    // // round long decimals
+    // result = Math.round(result * 1000) / 1000;
+
+    updateDisplayValue(result);
+    firstNumber = result;
+    operatorSelected = null;
+}
+
+function clearDisplay() {
+    updateDisplayValue("0");
+    firstNumber = "";
+    secondNumber = "";
+    operatorSelected = null;
+    shouldResetDisplay = false;
+}
+
+function backspace() {
+    if (shouldResetDisplay) return;
+    let currentValue = display.textContent;
+    console.log("in backspace", currentValue);
+    currentValue.length === 1 ? updateDisplayValue("0") : updateDisplayValue(currentValue.slice(0, -1));
+
+    // if (currentValue.length === 1) {
+    //     updateDisplayValue("0");
+    // } else {
+    //     updateDisplayValue(currentValue.slice(0, -1));
+    // }
 }
 
 document.querySelectorAll(".number").forEach(button => {
     button.addEventListener("click", () => appendNumber(button.textContent));
 });
+
+document.querySelectorAll(".operator").forEach(button => {
+    button.addEventListener("click", () => setOperator(button.textContent));
+});
+document.getElementById("equals").addEventListener("click", evaluate);
+document.querySelector(".clear").addEventListener("click", clearDisplay);
+document.querySelector(".backspace").addEventListener("click", backspace);
